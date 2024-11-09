f755583cdec9be2ae1a256a7c21229b7d7ca754e
Anton Nesterov <anton@demiurg.io>
2024-11-09 13:52:10 +0100

    alias e %:h/

--- init.lua
+++ init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.api.nvim_create_user_command("New", function(opts)
+  local cmd = "e %:h/" .. opts.args
+  vim.cmd(cmd)
+end, { nargs = "+", desc = "New file in current directory" })
